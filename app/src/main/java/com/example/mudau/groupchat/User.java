de175fb0124b8b6c135e52d4af5075a80fa5acdf
MasheshaDIgitalInnovators <31695029+MasheshaDIgitalInnovators@users.noreply.github.com>
2017-09-15 12:56:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mudau/groupchat/User.java
@@ -0,0 +1,47 @@
+package com.example.mudau.groupchat;
+
+/**
+ * Created by mudau on 9/15/2017.
+ */
+
+public class User {
+
+    String email;
+    String password;
+    String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public User(String email,String password,String name)
+    {
+        this.email=email;
+        this.password=password;
+        this.name=name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
